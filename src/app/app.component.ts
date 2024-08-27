13e82933b76e9137463825ac1174a8caa010a53d
tanapat muijeen <tanapat.muijeen@gmail.com>
2024-08-27 13:21:33 +0700

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private Nav: NavController) {}
+
+  AboutPage(){
+    this.Nav.navigateForward('/about')
+  }
 }
